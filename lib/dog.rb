5b491e2910a5b84a400a7eff78bb87845b801858
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-30 21:52:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+require 'pry'
+
+class Dog
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    @@all.each {|dog| puts dog}
+  end
+  binding.pry
+  
+  # def save
+  #   @@all << @name
+  # end
+  
+  # def 
+  
+end
